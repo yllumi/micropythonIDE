662b20e07e087ad3437200f7c3ef9fa4f391c067
Toni Haryanto <toha.samba@gmail.com>
2015-04-21 15:21:57 +0700

    read and write file success

--- assets/js/main.js
+++ assets/js/main.js
@@ -94,44 +94,148 @@ SerialConnection.prototype.disconnect = function() {
 ////////////////////////////////////////////////////////
 //// READ AND WRITE TO FILE ////////////////////////////
 
-var chosenFileEntry = null;
+
+var chosenEntry = null;
+var chooseFileButton = '.btn-flash';
+var saveFileButton = '.btn-save';
+var output = document.querySelector('output');
+var textarea = '#thecode';
 
 function errorHandler(e) {
   console.error(e);
 }
 
-// open file
-$(document).on('click', '.btn-flash', function(e){
-	chrome.fileSystem.chooseEntry({type: 'openFile'}, function(readOnlyEntry) {
+function readAsText(fileEntry, callback) {
+  fileEntry.file(function(file) {
+    var reader = new FileReader();
 
-		readOnlyEntry.file(function(file) {
-			var reader = new FileReader();
+    reader.onerror = errorHandler;
+    reader.onload = function(e) {
+      callback(e.target.result);
+    };
 
-			reader.onerror = errorHandler;
-			reader.onloadend = function(e) {
-				$('#code-editor').children('textarea').html(e.target.result);
-			};
+    reader.readAsText(file);
+  });
+}
 
-			reader.readAsText(file);
-		});
+function writeFileEntry(writableEntry, opt_blob, callback) {
+  if (!writableEntry) {
+    output.textContent = 'Nothing selected.';
+    return;
+  }
 
-		chosenFileEntry = readOnlyEntry;
-	});
+  writableEntry.createWriter(function(writer) {
+
+    writer.onerror = errorHandler;
+    writer.onwriteend = callback;
+
+    // If we have data, write it to the file. Otherwise, just use the file we
+    // loaded.
+    if (opt_blob) {
+      writer.truncate(opt_blob.size);
+      waitForIO(writer, function() {
+        writer.seek(0);
+        writer.write(opt_blob);
+      });
+    } 
+    else {
+      chosenEntry.file(function(file) {
+        writer.truncate(file.fileSize);
+        waitForIO(writer, function() {
+          writer.seek(0);
+          writer.write(file);
+        });
+      });
+    }
+  }, errorHandler);
+}
+
+function waitForIO(writer, callback) {
+  // set a watchdog to avoid eventual locking:
+  var start = Date.now();
+  // wait for a few seconds
+  var reentrant = function() {
+    if (writer.readyState===writer.WRITING && Date.now()-start<4000) {
+      setTimeout(reentrant, 100);
+      return;
+    }
+    if (writer.readyState===writer.WRITING) {
+      console.error("Write operation taking too long, aborting!"+
+        " (current writer readyState is "+writer.readyState+")");
+      writer.abort();
+    } 
+    else {
+      callback();
+    }
+  };
+  setTimeout(reentrant, 100);
+}
+
+// for files, read the text content into the textarea
+function loadFileEntry(_chosenEntry) {
+  chosenEntry = _chosenEntry;
+
+  chosenEntry.file(function(file) {
+    readAsText(chosenEntry, function(result) {
+      $(textarea).val(result);
+    });
+    // Update display.
+    saveFileButton.disabled = false; // allow the user to save the content
+  });
+}
+
+function loadInitialFile(launchData) {
+  if (launchData && launchData.items && launchData.items[0]) {
+    loadFileEntry(launchData.items[0].entry);
+  } 
+  else {
+    // see if the app retained access to an earlier file or directory
+    chrome.storage.local.get('chosenFile', function(items) {
+      console.log(items);
+      if (items.chosenFile) {
+        // if an entry was retained earlier, see if it can be restored
+        chrome.fileSystem.isRestorable(items.chosenFile, function(bIsRestorable) {
+          // the entry is still there, load the content
+          console.info("Restoring " + items.chosenFile);
+          chrome.fileSystem.restoreEntry(items.chosenFile, function(chosenEntry) {
+            if (chosenEntry) {
+              chosenEntry.isFile ? loadFileEntry(chosenEntry) : loadDirEntry(chosenEntry);
+            }
+          });
+        });
+      }
+    });
+  }
+}
+
+$(document).on('click', chooseFileButton, function(e) {
+  var accepts = [{
+    mimeTypes: ['text/*'],
+    extensions: ['py']
+  }];
+  chrome.fileSystem.chooseEntry({type: 'openFile', accepts: accepts}, function(theEntry) {
+    if (!theEntry) {
+      output.textContent = 'No file selected.';
+      return;
+    }
+    // use local storage to retain access to this file
+    chrome.storage.local.set({'chosenFile': chrome.fileSystem.retainEntry(theEntry)});
+    loadFileEntry(theEntry);
+  });
 });
 
-// save file
-$(document).on('click', '.btn-save', function(e){
-	chrome.fileSystem.getWritableEntry(chosenFileEntry, function(writableFileEntry) {
-		writableFileEntry.createWriter(function(writer) {
-			writer.onerror = errorHandler;
-			writer.onwriteend = callback;
-
-			chosenFileEntry.file(function(file) {
-				writer.write(file);
-			});
-		}, errorHandler);
-	});
+$(document).on('click', saveFileButton, function(e) {
+  // var config = {type: 'saveFile', suggestedName: chosenEntry.name};
+  // chrome.fileSystem.chooseEntry(config, function(writableEntry) {
+    var blob = new Blob([$(textarea).val()], {type: 'text/plain'});
+    writeFileEntry(chosenEntry, blob, function(e) {
+      output.textContent = 'Write complete :)';
+    });
+  // });
 });
+
+loadInitialFile(launchData);
+
 
 ////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////
--- launch.js
+++ launch.js
@@ -1,10 +1,13 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('index.html', {
-  	id: "mainwin",
-  	frame: "none",
-    'bounds': {
-      'width': 1024,
-      'height': 768
-    }
-  });
+chrome.app.runtime.onLaunched.addListener(function(launchData) {
+	chrome.app.window.create('index.html', {
+		id: "mainwin",
+		frame: "none",
+		'bounds': {
+			'width': 1024,
+			'height': 768
+		}
+	},
+	function(win) {
+		win.contentWindow.launchData = launchData;
+	});
 });
